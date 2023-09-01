e86ae4bc02ee24f0729f4b0486e8e7711351898d
vmeijssen <vossemeijssen@hotmail.com>
2023-09-01 15:50:57 +0200

    Add functionality for periodic BC

--- test.py
+++ test.py
@@ -3,19 +3,29 @@ from tqdm import tqdm
 
 # Settings
 dt = 0.001 # Also known as k
-dx = 0.001 # Also known as h
+xmin = -1 
+xmax = 1
+xlen = 2000
 T = 1
+periodic_BC = True
 
 # Set up variables
-x = np.arange(
-    start=  -1,
-    stop=   1+dx,
-    step=   dx,
+# x = np.arange(
+#     start=  -1,
+#     stop=   1+dx,
+#     step=   dx,
+# )
+x = np.linspace(
+    start=xmin,
+    stop=xmax,
+    num=xlen
 )
+dx = (xmax - xmin) / (xlen - 1) # Also known as h
+
 q = np.zeros_like(x)
 
 # Set initial values
-q[:int(len(q)/2)] = 0.75
+q[:int(len(q)/2)] = 0.5
 q[int(len(q)/2):] = 0.25
 
 # Choose FR
@@ -29,29 +39,33 @@ for timestep in tqdm(range(timesteps)):
 
     # Find all q* (or actually, find all f(q*))
     f_q_star = np.zeros_like(q)
-    for i in range(len(f_q_star)-1):
+    for i in range(xlen):
         # When looking at q*[i], we need info from q[i] and q[i]+1
         # Edge case at i_max: in this case, q[i_max] will be constant
         # Four cases:
-        if f_der[i] >= 0 and f_der[i+1] >= 0:
+        if f_der[i] >= 0 and f_der[(i+1)%xlen] >= 0:
             f_q_star[i] = f[i]
-        elif f_der[i] < 0 and f_der[i+1] < 0:
-            f_q_star[i] = f[i+1]
-        elif f_der[i] >= 0 and f_der[i+1] < 0:
-            s = (f[i+1] - f[i])/(q[i+1] - q[i])
+        elif f_der[i] < 0 and f_der[(i+1)%xlen] < 0:
+            f_q_star[i] = f[(i+1)%xlen]
+        elif f_der[i] >= 0 and f_der[(i+1)%xlen] < 0:
+            s = (f[(i+1)%xlen] - f[i])/(q[(i+1)%xlen] - q[i])
             if s >= 0:
                 f_q_star[i] = f[i]
             else:
-                f_q_star[i] = f[i+1]
-        elif f_der[i] < 0 and f_der[i+1] >= 0:
+                f_q_star[i] = f[(i+1)%xlen]
+        elif f_der[i] < 0 and f_der[(i+1)%xlen] >= 0:
             f_q_star[i] = fr.fmax
 
     # Now that q* is known, we can calculate the new q values
     new_q = np.zeros_like(q)
-    new_q[0] = q[0]
-    new_q[-1] = q[-1]
-    for i in range(1, len(q)-1):
-        new_q[i] = q[i] - dt / dx * (f_q_star[i] - f_q_star[i-1])
+    if periodic_BC:
+        for i in range(xlen):
+            new_q[i] = q[i] - dt / dx * (f_q_star[i] - f_q_star[(i-1)%xlen])
+    else:
+        new_q[0] = q[0]
+        new_q[-1] = q[-1]
+        for i in range(1, xlen-1):
+            new_q[i] = q[i] - dt / dx * (f_q_star[i] - f_q_star[i-1])
 
     q = new_q
 
